f20c9eb5f8c999959520acaa81c9bd75f0ea395e
El Coyote Cojo <El Coyote Cojo@Minerva>
2020-08-14 20:50:46 +0200

    Agenda: Added plugin to list available plugins. Update log view if log file changes

--- pclg-agenda/src/main/java/org/pclg/agenda/gui/actions/ViewLogAction.java
+++ pclg-agenda/src/main/java/org/pclg/agenda/gui/actions/ViewLogAction.java
@@ -5,6 +5,8 @@ import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
 import org.pclg.agenda.gui.AgendaGUI;
 import org.pclg.gui.JTabbedPaneWithCloseIcons;
+import org.pclg.gui.ManagedScrollPane;
+import org.pclg.gui.ScrollBarManager;
 import org.pclg.log.LoggerFactory;
 import org.pclg.tools.FileTools;
 
@@ -14,8 +16,14 @@ import javax.swing.JTextArea;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.stream.Collectors;
 
 /**
  * @since 15/03/2018.
@@ -25,7 +33,7 @@ public class ViewLogAction extends AbstractAction {
     private final AgendaGUI agendaGUI;
     private final JTabbedPaneWithCloseIcons tabbedPane;
 
-    public ViewLogAction(AgendaGUI agendaGUI, final JTabbedPaneWithCloseIcons tabbedPane) {
+    public ViewLogAction(final AgendaGUI agendaGUI, final JTabbedPaneWithCloseIcons tabbedPane) {
         this.agendaGUI = agendaGUI;
         this.tabbedPane = tabbedPane;
     }
@@ -34,94 +42,78 @@ public class ViewLogAction extends AbstractAction {
     public void actionPerformed(final ActionEvent ev) {
         final JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
-        final Enumeration allAppenders = Logger.getRootLogger().getAllAppenders();
+        final Enumeration<?> allAppenders = Logger.getRootLogger().getAllAppenders();
         final StringBuilder fileNames = new StringBuilder();
-        while (allAppenders.hasMoreElements()){
-            final Appender appender = (Appender) allAppenders.nextElement();
-            if (appender instanceof FileAppender){
-                final String fileName = ((FileAppender) appender).getFile();
-                fileNames.append(fileName).append(", ");
-                textArea.append("File: " + fileName + '\n');
-                try {
-                    textArea.append(new String(FileTools.readFromFile(fileName)) + '\n');
-                } catch (IOException ex) {
-                    LOGGER.error(LoggerFactory.ERROR_TAG, ex);
-                    textArea.append(ex.toString() + '\n');
-                }
-                textArea.append("=================================================\n");
-            }
-		}
+        class FileControl {
+            final File file;
+            long lastModified;
 
-		class ClosableJScrollPane extends JScrollPane implements ActionListener {
-			private ClosableJScrollPane(Component textArea) {
-                super(textArea);
+            FileControl(final File file) {
+                this.file = file;
+                lastModified = Long.MIN_VALUE;
+            }
+
+            public File getFile() {
+                return file;
+            }
+        }
+        final List<FileControl> logFiles = new ArrayList<>();
+        while (allAppenders.hasMoreElements()) {
+            final Appender appender = (Appender) allAppenders.nextElement();
+            if (appender instanceof FileAppender) {
+                final String fileName = ((FileAppender) appender).getFile();
+                logFiles.add(new FileControl(new File(fileName)));
+                fileNames.append(fileName).append(", ");
+            }
+        }
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                final boolean[] somethingMustBeDone = {false};
+                logFiles.forEach(fileControl -> {
+                    final long lastModified = fileControl.file.lastModified();
+                    if (fileControl.lastModified != lastModified) {
+                        fileControl.lastModified = lastModified;
+                        somethingMustBeDone[0] = true;
+                    }
+                });
+                if (somethingMustBeDone[0]) {
+                    showLogs(textArea, logFiles.stream().map(FileControl::getFile).collect(Collectors.toList()));
+                    somethingMustBeDone[0] = false;
+                }
+            }
+            
+            private void showLogs(final JTextArea textArea, final List<File> logFiles) {
+                    textArea.setText("");
+                    logFiles.forEach(file -> {
+                        final String fileName = file.getAbsolutePath();
+                        textArea.append("File: " + fileName + '\n');
+                        try {
+                            textArea.append(new String(FileTools.readFromFile(fileName)) + '\n');
+                        } catch (final IOException ex) {
+                            LOGGER.error(LoggerFactory.ERROR_TAG, ex);
+                            textArea.append(ex.toString() + '\n');
+                        }
+                        textArea.append("=================================================\n");
+                    });
+                }
+        }, 100, 1000);
+
+        class ClosableJScrollPane extends ManagedScrollPane implements ActionListener {
+            private ClosableJScrollPane(final Component textArea) {
+                super(textArea, ScrollBarManager.Position.BOTTOM);
             }
 
             /** Action a ejecutar por el aspa del TabPanel. */
             @Override
             public void actionPerformed(final ActionEvent e) {
+                timer.cancel();
                 tabbedPane.remove(this);
             }
         }
 
         agendaGUI.add2TabbedPane(fileNames.toString(), new ClosableJScrollPane(textArea));
-
-//		textArea.append("=======================   ClassPath     ==========================\n");
-//		final URL[] classPath = ClassPathHacker.getClassPath();
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		final PrintStream printStream = new PrintStream(baos);
-//		ArrayTools.printArray(printStream, classPath);
-//		textArea.append(baos.toString());
-//		for (URL url : classPath) {
-//			final File path = new File(url.getPath());
-//			final File path = getExecutionPath();
-//			final File path = new File("C:\\home\\development\\build");
-//			System.err.println(">> Plugins disponibles en " + path + " <<");
-//	  		final Class<?>[] clases = ToolBox.findClasses(Plugin.class, path);
-//			for (Class<?> clase : clases) {
-//				System.err.println("clase = " + clase);
-//			}
-//
-//		}
-
-/*
-		// El problema con este approach es que instancia todas las clases que haya por el camino. FIXME: Buscar otra solución
-        final File path = getExecutionPath(Agenda.class);
-        System.err.println(">> Plugins disponibles en " + path + " <<");
-        final Class[] clases = ToolBox.findClasses(Plugin.class, path);
-        for (Class clase : clases) {
-            System.err.println("clase = " + clase);
-        }
-
- */
-	}
-
-
-	/**
-	 * Determina de donde se está ejecutando la aplicacion: un directorio o un jar
-	 */
-//	private File getExecutionPath(final Class myClass) {
-//		String myName = myClass.getName();
-//		myName = myName.substring(myName.lastIndexOf('.') + 1);
-//
-//		final URL appURL = myClass.getResource(myName + ".class");
-//		if (appURL == null) {
-//			System.err.println("No puedo determinar el punto de ejecucion");
-//			return null;
-//		}
-//
-//		// Determinar que jar o directorio se está ejecutando
-//		final String path = appURL.getPath();
-//		int end = path
-//			.lastIndexOf('!');        // Si es un jar, termina con ! y el nombre de la clase
-//		if (end == -1) {
-//			end = path.lastIndexOf(
-//				'/');        // Si es un directorio, termina con / y el nombre de la clase
-//		}
-//		final String pathStr = path.substring(path.indexOf('/') + 1, end);
-//		final String aPackage = myClass.getPackage().getName().replace('.', '/');
-//		final int indexOf = pathStr.indexOf(aPackage);
-//		final String baseDir = pathStr.substring(0, indexOf);
-//		return new File(baseDir);
-//	}
+    }
 }
